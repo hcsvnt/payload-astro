631ba4588a556acbe221b25397cf4e602dcbd12b
Mateusz Hallala <matt@huncwot.com>
2025-01-16 14:29:53 +0100

    update: tidy up astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,25 +5,21 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-      svg: {
-          mode: 'sprite', // or 'inline'
-      },
-  },
+    experimental: {
+        svg: {
+            mode: 'sprite', // or 'inline'
+        },
+    },
+    i18n: {
+        locales: ['en-US', 'pl-PL', 'de-DE', 'es-ES'],
+        defaultLocale: 'pl-PL',
+    },
+    output: 'server', // 'server' for SSR or 'static' for SSG
+    vite: {
+        ssr: {
+            noExternal: ['webcoreui'],
+        },
+    },
 
-  i18n: {
-      locales: ['en-US', 'pl-PL', 'de-DE', 'es-ES'],
-      defaultLocale: 'pl-PL',
-  },
-
-  // 'server' for SSR or 'static' for SSG
-  output: 'server',
-
-  vite: {
-      ssr: {
-          noExternal: ['webcoreui'],
-      },
-  },
-
-  adapter: vercel(),
+    adapter: vercel(),
 });
